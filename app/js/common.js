ae609adc6cc08398e56214bb25f2c987c6b0f638
oleg <oleg.abalyaev@gmail.com>
2018-12-03 09:36:24 +0300

    preproduction

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,11 @@
-$(function() {
+$(function () {
 
-	// Custom JS
+    // Custom JS
+    var menu = document.querySelector(".navigation-wrapper")
+    var dropdownbButton = document.querySelector(".dropdown-button")
+    dropdownbButton.addEventListener("click", function () {
+        menu.classList.toggle("navigation-wrapper-hidden");
+    })
+
 
 });
