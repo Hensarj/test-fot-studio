a0577a87e1ae1d67e22c1ed19da730fac54d7c9d
oleg <oleg.abalyaev@gmail.com>
2018-12-03 11:01:32 +0300

    preproduction

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,55 @@
 $(function () {
 
     // Custom JS
-    var menu = document.querySelector(".navigation-wrapper")
-    var dropdownbButton = document.querySelector(".dropdown-button")
+    var menu = document.querySelector(".navigation-wrapper");
+    var dropdownbButton = document.querySelector(".dropdown-button");
     dropdownbButton.addEventListener("click", function () {
         menu.classList.toggle("navigation-wrapper-hidden");
     })
+    var form = document.querySelector(".payment-info__card");
+    console.log(form.elements.cardCodeNumber);
+    form.elements.numberBlock1.addEventListener("keypress", function (e) {
+        if (!checkInput(e, "number")) {
+            e.preventDefault();
+        }
+    })
+    form.elements.numberBlock2.addEventListener("keypress", function (e) {
+        if (!checkInput(e, "number")) {
+            e.preventDefault();
+        }
+    })
+    form.elements.numberBlock3.addEventListener("keypress", function (e) {
+        if (!checkInput(e, "number")) {
+            e.preventDefault();
+        }
+    })
+    form.elements.numberBlock4.addEventListener("keypress", function (e) {
+        if (!checkInput(e, "number")) {
+            e.preventDefault();
+        }
+    })
+    form.elements.verificationCodeValue.addEventListener("keypress", function (e) {
+        if (!checkInput(e, "number")) {
+            e.preventDefault();
+        }
+    })
+    form.elements.cardHolder.addEventListener("keypress", function (e) {
+        if (!checkInput(e, "name")) {
+            e.preventDefault();
+        }
+    })
+
+    function checkInput(ew, compare) {
+        var ew = event.which;
+        if (compare == "number")
+            if (48 <= ew && ew <= 57)
+                return true;
+        if (compare == "name")
+            if ((65 <= ew && ew <= 90) || (97 <= ew && ew <= 122) || (ew == 32))
+                return true;
+        return false;
+    }
 
 
-});
+})
+;
